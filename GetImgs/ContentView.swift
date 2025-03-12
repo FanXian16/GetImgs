9c2dd4b112c7480fb3c585335cb736fe68125e90
FanXian <fxean@icloud.com>
2025-03-12 23:28:09 +0800

    Initial Commit

---
+++ GetImgs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetImgs
+//
+//  Created by 范贤 on 2025/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetImgs/GetImgsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetImgsApp.swift
+//  GetImgs
+//
+//  Created by 范贤 on 2025/3/12.
+//
+
+import SwiftUI
+
+@main
+struct GetImgsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
